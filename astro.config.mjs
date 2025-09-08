59896523bb5121c09e90512802c2eba1db2dd3f0
parkerpixie <parkerpixiedust@gmail.com>
2025-09-08 17:25:34 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Keep it minimal. Don't pass a custom `vite` block unless we truly need it.
-  // Astro will provide a working Vite config on Netlify.
   output: 'static'
+  // ⬆️ Keep it minimal. No custom vite block.
 });
